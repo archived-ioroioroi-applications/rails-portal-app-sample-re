89d66b36c91c926e1bc31b1e07e1a19f46685b5c
ioroioroi <kazuya.ioroi@gmail.com>
2018-03-25 15:02:54 +0900

    Optimisation for internationalization - config/locales で文言管理できるように設定 - top/index.html の文言を早速ロケールファイル使って表示 - デフォルトの言語を日本語に設定

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module Myportal
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Tokyo'
+
+    config.i18n.default_locale = :ja
   end
 end
